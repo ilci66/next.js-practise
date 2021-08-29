4faf314bff48b1a2986d753d044fe8e011a90637
unknown <akbiyik.ilker7@gmail.com>
2021-08-29 19:49:34 +0300

    add testing routes

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Homepage</h1>
-      <p>Next.js pre renders and sends to browser</p>
-      <p>Duis sunt nulla Lorem et nulla sit dolor sunt aliqua eiusmod sit magna labore in. Mollit in incididunt id id 
+// next.js automatically creates the route /about for his page when I create the file
+const About = () => {
+	return (
+	  <div>
+			<h1>About</h1>
+			<p>some text</p>
+			<p>Duis sunt nulla Lorem et nulla sit dolor sunt aliqua eiusmod sit magna labore in. Mollit in incididunt id id 
       aliquip duis. Voluptate est ad sit id magna nisi proident ex eiusmod aliqua amet elit veniam mollit.
       Elit tempor fugiat eiusmod qui do nisi ipsum dolore sunt aliqua. Dolor nostrud ad exercitation magna Lorem est 
       esse deserunt cillum amet culpa nisi magna deserunt. Fugiat id esse velit amet duis consectetur in mollit 
       occaecat laboris magna. Duis eiusmod laborum ad excepteur cillum sit culpa sunt in aliquip occaecat Lorem 
       ullamco.</p>
-    </div>
+		</div>
   )
 }
+
+
+export default About;
---
+++ pages/test/testing.js
@@ -0,0 +1,9 @@
+const Testing = () => {
+	return(
+		<div>
+			<h1>Testing the next.js routing</h1>
+		</div>
+	)
+}
+
+export default Testing;
