a1126f56aad505e66811e0a7e0942b071c3c095b
unknown <akbiyik.ilker7@gmail.com>
2021-08-30 14:52:19 +0300

    add more styles

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Next.js pre renders and sends to browser</p>
-      <p>Duis sunt nulla Lorem et nulla sit dolor sunt aliqua eiusmod sit magna labore in. Mollit in incididunt id id 
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Next.js pre renders and sends to browser</p>
+      <p className={styles.text}>Duis sunt nulla Lorem et nulla sit dolor sunt aliqua eiusmod sit magna labore in. Mollit in incididunt id id 
       aliquip duis. Voluptate est ad sit id magna nisi proident ex eiusmod aliqua amet elit veniam mollit.
       Elit tempor fugiat eiusmod qui do nisi ipsum dolore sunt aliqua. Dolor nostrud ad exercitation magna Lorem est 
       esse deserunt cillum amet culpa nisi magna deserunt. Fugiat id esse velit amet duis consectetur in mollit 
