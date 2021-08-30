fdf7a6e4e679e7eb4689a2a359b93ac2e850bf2e
unknown <akbiyik.ilker7@gmail.com>
2021-08-30 15:02:28 +0300

    added custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+//this page will be served instead of typical 404 page
+import Link from 'next/link'
+
+const notFound = () => {
+    return (
+        <div className="not-found">
+            <h1>Ooops...</h1>
+            <h2>That page cannot be found :(</h2>
+            <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
+        </div>
+      );
+}
